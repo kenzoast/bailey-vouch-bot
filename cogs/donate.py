451c71f34b16aa736a9ecc3ea07c181f65430fe0
Scubasservices <scubasservices@outlook.com>
2024-12-04 11:18:32 -0500

    added sigma list for 1$
    sigma sigma on the wall whos the skibidiest of them all

--- cogs/donate.py
+++ cogs/donate.py
@@ -10,7 +10,8 @@ class Donation(commands.Cog):
         donation_info = (
             "💰 **Paypal Donations:** for any paypal donations you can donate to scubasservices @outlook.com but we much perfer crypto donations\n"
             "💳 **Crypto Donations:** To donate with crypto send to this address: LKRM3Nii8WwuWTturgh6nq41zuZfgnficZ)\n"
-            "**Custom roles:** If you donate more than 5$ to the crypto wallet or 10$ to the paypal account you can get your own custom role with your own custom color!"
+            "**Custom roles:** If you donate more than 5$ to the crypto wallet or 10$ to the paypal account you can get your own custom role with your own custom color!\n"
+            "**/skibidi list:** If you donate more than 1$ to either you can get on the sigma list!"
         )
         await ctx.respond(donation_info, ephemeral=True)
 
