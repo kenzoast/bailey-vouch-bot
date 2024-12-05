f5bde6b8af7943adcf621201c3b318ae39efd8e3
Scubasservices <scubasservices@outlook.com>
2024-12-05 11:25:48 -0500

    whoops

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ bot.load_extension('cogs.rockpaperscissors')
 bot.load_extension('cogs.skibidi')
 bot.load_extension('cogs.coinflip')
 bot.load_extension('cogs.avatar')
-bot.load_extension('cogs.userinfo')
+bot.load_extension('cogs.info')
 
 # Run the main bot
 bot.run(TOKEN)
