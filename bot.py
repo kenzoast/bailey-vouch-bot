7dd1cc1bfc4aed4915eca59197d2dde0ad6c552d
Scubasservices <scubasservices@outlook.com>
2024-12-09 14:40:00 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,6 +84,7 @@ bot.load_extension('cogs.eightball')
 bot.load_extension('cogs.fishing')
 bot.load_extension('cogs.blackjack')
 bot.load_extension('cogs.stickymessage')
+bot.load_extension("cogs.embedbuilder")
 
 # Run the main bot
 bot.run(TOKEN)
